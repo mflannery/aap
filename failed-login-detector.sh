ecbe8557d308ed7415c10a66f931ea6a66b1c501
mike <mflanner@redhat.com>
2025-03-19 17:47:38 -0400

    Updated login-detector.sh

--- failed-login-detector.sh
+++ failed-login-detector.sh
@@ -9,7 +9,8 @@ journalctl -u sshd -f | while read -r line; do
     if echo "$line" | grep -iq "Failed password"; then
         echo "Failed login attempt detected: $line"
         # Execute the command
-        JSON_STRING=$(jq -n --arg login "failed" '$ARGS.named' --arg ipaddress "192.168.2.1" 'ARGS.named')
+        IP=$(echo "$line" | sed 's/.*from \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/')
+        JSON_STRING=$(jq -n --arg login "failed" '$ARGS.named' --arg ipaddress "$IP" 'ARGS.named')
         curl -X POST -H "Authorization:Bearer VMbe5JIf9#!Z%t" -H "Content-Type: application/json" -d "$JSON_STRING" --insecure https://aap25.home.io:443/eda-event-streams/api/eda/v1/external_event_stream/c41d74d7-2ffb-4a43-af75-41ab251abb2c/post/
     fi
 done
