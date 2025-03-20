96fe60e1a95e0f07cc592506f89003f29913b328
mike <mflanner@redhat.com>
2025-03-19 20:22:21 -0400

    fixed if statement

--- check-fs.sh
+++ check-fs.sh
@@ -10,7 +10,8 @@ while true;
 do
     # Get the filesystem usage percentage
     USAGE=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
-    if [ "$USAGE" -gt 80 ]; then
+    if [ "$USAGE" -gt 80 ]; 
+    then
         # Use logger to write usage to system logs
         logger -p user.warning "High filesystem usage: $USAGE% of root filesystem is full"
         #Build the JSON string/object
@@ -19,4 +20,5 @@ do
 #       curl -X POST -H "Content-Type: application/json" -d "$JSON_STRING" aap25.home.io:3001/endpoint
 #       curl -X POST -H "Authorization:Bearer redhat" -H "Content-Type: application/json" -d "$JSON_STRING" aap25.home.io:3001/endpoint
         curl -X POST -H "Authorization:Bearer VMbe5JIf9#!Z%t" -H "Content-Type: application/json" -d "$JSON_STRING" --insecure https://aap25.home.io:443/eda-event-streams/api/eda/v1/external_event_stream/c41d74d7-2ffb-4a43-af75-41ab251abb2c/post/
+    fi
 done
