10ec6cfa258319ba6e9413c26afadf3f87d46b11
Mike Flannery <michael.flannery@gmail.com>
2025-04-18 14:39:04 -0400

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -10,18 +10,18 @@ journalctl -f | while read -r line; do
         *"Failed password"*)
         echo "Failed login attempt detected: $line"
         IP=$(echo "$line" | sed 's/.*from \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\).*/\1/')
-        JSON_STRING=$(jq -n --arg login "ssh-failed" '$ARGS.named' --arg ipaddress "$IP" 'ARGS.named')
+        JSON_STRING=$(jq -n --arg error "ssh-failed" '$ARGS.named' --arg ipaddress "$IP" 'ARGS.named')
         ;;
 
         *"incorrect password"*)
         echo "Failed sudo attempt detected: $line"
-        JSON_STRING=$(jq -n --arg login "sudo-failed" '$ARGS.named' --arg log "$line" 'ARGS.named')
+        JSON_STRING=$(jq -n --arg error "sudo-failed" '$ARGS.named' --arg log "$line" 'ARGS.named')
         ;;
 
         *COMMAND*etc*)
         echo "Watched file edited $line"
-        JSON_STRING=$(jq -n --arg login "file-edited" '$ARGS.named' --arg log "$line" 'ARGS.named')
+        JSON_STRING=$(jq -n --arg error "file-edited" '$ARGS.named' --arg log "$line" 'ARGS.named')
         ;;
     esac
-#        curl -X POST -H "Authorization:Bearer VMbe5JIf9#!Z%t" -H "Content-Type: application/json" -d "$JSON_STRING" --insecure https://aap25.home.io:443/eda-event-streams/api/eda/v1/external_event_stream/c41d74d7-2ffb-4a43-af75-41ab251abb2c/post/
+    curl -X POST -H "Authorization:Bearer VMbe5JIf9#!Z%t" -H "Content-Type: application/json" -d "$JSON_STRING" --insecure https://aap25.home.io:443/eda-event-streams/api/eda/v1/external_event_stream/c41d74d7-2ffb-4a43-af75-41ab251abb2c/post/
 done
